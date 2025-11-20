03cb5234aeaa7919733df8dd4e0a9b0c4f1831ed
Christopher-Lord <lordc42@gmail.com>
2025-11-20 11:25:20 +0200

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="podcast-header">
+      <img src="./src/assets/podcast-logo-light.png" alt="Podcast Logo" />
+      <h1>PodcastApp</h1>
+    </header>
+  );
+}
