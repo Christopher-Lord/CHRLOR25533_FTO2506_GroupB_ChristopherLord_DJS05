385c0ee203ad84dd8f9a30212c17fc31d520e8db
Christopher-Lord <lordc42@gmail.com>
2025-11-22 10:46:39 +0200

    Added Route link to component

--- src/components/PodcastCard.jsx
+++ src/components/PodcastCard.jsx
@@ -1,5 +1,6 @@
 import { format } from "date-fns";
 import "./PodcastCard.css";
+import { Link } from "react-router-dom";
 
 /**
  * React component that creates a single podcast card
@@ -9,27 +10,29 @@ import "./PodcastCard.css";
  */
 export default function PodcastCard({ podcast }) {
   // Destructure podcast props for cleaner access
-  const { image, title, seasons, genres, updated } = podcast;
+  const { id, image, title, seasons, genres, updated } = podcast;
 
   // Format the "updated" date into a readable format
   const formattedDate = format(new Date(updated), "MMM d, yyyy");
 
   return (
-    <div className="podcast-card">
-      <img src={image} alt={title} className="cover-img" />
+    <Link to={`/show/${id}`} className="link">
+      <div className="podcast-card">
+        <img src={image} alt={title} className="preview-cover-img" />
 
-      <h2>{title}</h2>
+        <h2>{title}</h2>
 
-      <p className="podcast-seasons">{seasons} seasons</p>
+        <p className="podcast-seasons">{seasons} seasons</p>
 
-      <ul className="podcast-genres">
-        {/* Loop through the array of genres and render each one as a list item */}
-        {genres.map((genre) => (
-          <li key={genre}>{genre}</li>
-        ))}
-      </ul>
+        <ul className="podcast-genres">
+          {/* Loop through the array of genres and render each one as a list item */}
+          {genres.map((genre) => (
+            <li key={genre}>{genre}</li>
+          ))}
+        </ul>
 
-      <p className="podcast-updated">Updated: {formattedDate}</p>
-    </div>
+        <p className="podcast-updated">Updated: {formattedDate}</p>
+      </div>
+    </Link>
   );
 }
