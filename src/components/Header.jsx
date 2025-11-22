c794bf27c7f9426a8b80cd48e72dc09ec27774ae
Christopher-Lord <lordc42@gmail.com>
2025-11-22 10:45:15 +0200

    Update image src

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="podcast-header">
-      <img src="./src/assets/podcast-logo-light.png" alt="Podcast Logo" />
+      <img src="/src/assets/podcast-logo-light.png" alt="Podcast Logo" />
       <h1>PodcastApp</h1>
     </header>
   );
